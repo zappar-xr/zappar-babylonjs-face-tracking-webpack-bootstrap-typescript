caf99eb19bb5cbb52500b0a7fc5b2bc2904e60ee
Zappar <hello@zappar.com>
2021-07-26 12:26:43 +0100

    Minimal webpack project for Zappar BabylonJS face tracking (TypeScript)

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+import * as BABYLON from 'babylonjs';
+import * as ZapparBabylon from '@zappar/zappar-babylonjs';
+
+import "./style.css";
+
+// Setup BabylonJS in the usual way
+const canvas = document.createElement('canvas');
+document.body.appendChild(canvas);
+
+const engine = new BABYLON.Engine(canvas, true, {
+    preserveDrawingBuffer: true,
+    stencil: true
+});
+
+export const scene = new BABYLON.Scene(engine);
+const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
+
+// Setup a Zappar camera instead of one of Babylon's cameras
+export const camera = new ZapparBabylon.Camera('camera', scene);
+
+// Request the necessary permission from the user
+ZapparBabylon.permissionRequestUI().then((granted) => {
+    if (granted) camera.start(true);
+    else ZapparBabylon.permissionDeniedUI();
+});
+
+// Set up our image tracker transform node
+const faceTracker = new ZapparBabylon.FaceTrackerLoader().load();
+const trackerTransformNode = new ZapparBabylon.FaceTrackerTransformNode('tracker', camera, faceTracker, scene);
+
+const material = new BABYLON.StandardMaterial('mat', scene);
+material.diffuseTexture = new BABYLON.Texture(require("file-loader!./faceMeshTemplate.png").default, scene);
+
+// Face mesh
+const faceMesh = new ZapparBabylon.FaceMeshGeometry('mesh', scene);
+faceMesh.parent = trackerTransformNode;
+faceMesh.material = material;
+
+
+
+window.addEventListener('resize', () => {
+    engine.resize();
+});
+
+// Set up our render loop
+engine.runRenderLoop(() => {
+    faceMesh.updateFromFaceTracker(faceTracker);
+    camera.updateFrame();
+    scene.render();
+});
